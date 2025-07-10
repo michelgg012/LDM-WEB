9574cba21f556cd22f8bf80ceb55fedc43f9143b
michelgg012 <michelg.work@gmail.com>
2025-07-10 08:41:51 -0300

    first commit

--- src/Utils/Constants.js
+++ src/Utils/Constants.js
@@ -1,6 +1,6 @@
 export const ENV ={
   SERVER_HOST: 'http://localhost:3000',
-  API_HOST_PRUEBAS: 'http://192.168.100.7:3412',
+  API_HOST_PRUEBAS: 'http://192.168.1.10:3412',
   API_HOST_PRODUCCION: 'http://201.139.80.173:3412',
   TOKEN:'token',
   CART:'cart',
