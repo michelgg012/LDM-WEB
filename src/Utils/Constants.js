ea2e00e49a46a342f5ff5ca08db08850af3fa3d7
Michel Garay <michelg.work@gmail.com>
2025-07-21 09:29:01 -0300

    update 21 julio

--- src/Utils/Constants.js
+++ src/Utils/Constants.js
@@ -1,7 +1,7 @@
 export const ENV ={
-  SERVER_HOST: 'http://localhost:3000',
-  API_HOST_PRUEBAS: 'http://192.168.1.10:3413',
-  API_HOST_PRODUCCION: 'http://201.139.80.173:3412',
+  SERVER_HOST: 'http://192.168.1.10:3412',
+  API_HOST_PRUEBAS: 'http://192.168.1.10:3412',
+  API_HOST_PRODUCCION: 'https://repartapp.site/api',
   TOKEN:'token',
   CART:'cart',
   SELECT_PRODUCT:'selectProducts',
